c6635df1994b53e05d4d5264c3c07ee347da47a3
Michele Sollecito <msollecito@meta.com>
2026-03-29 20:16:49 +0100

    WIP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 
 rootProject.name = "lattice"
 
+includeBuild("../gradle-plugins")
+
 fun module(vararg pathSegments: String) = subProject(rootFolder = "modules", pathSegments = pathSegments)
 
 fun subProject(rootFolder: String, vararg pathSegments: String, excludeRootFolderFromGroupName: Boolean = true) {
